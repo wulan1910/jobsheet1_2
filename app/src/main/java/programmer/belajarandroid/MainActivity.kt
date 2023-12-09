223664b0c66ba126343aae6fd48c4a5037960be8
wulan1910 <145314196+wulan1910@users.noreply.github.com>
2023-12-09 11:43:35 +0700

    jobsheet1_2

---
+++ app/src/main/java/programmer/belajarandroid/MainActivity.kt
@@ -0,0 +1,11 @@
+package programmer.belajarandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
